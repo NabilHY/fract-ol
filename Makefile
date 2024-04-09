9710d37bbd4505bc3034476cdca7eb0848782e48
Nabil <nabil.hayun@gmail.com>
2024-04-09 01:20:46 +0000

    Setup mlx

--- Makefile
+++ Makefile
@@ -1,12 +1,30 @@
 CC = cc
 FLAGS = -Wall -Wextra -Werror
+ENTRY = fractol.c
+OBJS = $(SRCS:objs/.o=.c)
 LIB_AR = ./libr/libr.a
+MLX_MACOS = -framework Cocoa -framework OpenGL -framework IOKit
+MLX_INCLUDE = ./mlx/include/MLX42.h
+GLFW = -lglfw -L/Users/nhayoun/.brew/opt/glfw/lib/
+GLFW_PATH = /users/nhayoun/.brew/opt/glfw/lib/
+MLX_AR = ./mlx/build/libmlx42.a
+MLXLIB = ./mlx/lib/MLX42
 FSANITIZE= -g -fsanitize=address
-LIB_NAME = libr.a
+NAME = fractol
 
 .PHONY: libr
 
-all: $(LIB_NAME)
+all: $(NAME)
+
+$(LIB_AR):
+	make -C libr
+
+$(NAME):
+	$(CC) $(FLAGS) $(ENTRY) $(LIB_AR) $(MLX_MACOS) $(MLX_AR) $(GLFW)   -o $(NAME)
+
+./objs/%.o: %.c
+	mkdir -p objs
+	$(CC) $(FLAGS) -I$(INCLUDE) -c $< -o $@
 
 $(LIB_NAME):
 	make -C libr
--- fractol.h
+++ fractol.h
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.c                                          :+:      :+:    :+:   */
+/*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhayoun <nhayoun@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/05 04:48:42 by nhayoun           #+#    #+#             */
-/*   Updated: 2024/04/06 11:05:44 by nhayoun          ###   ########.fr       */
+/*   Created: 2024/04/06 04:38:23 by nhayoun           #+#    #+#             */
+/*   Updated: 2024/04/06 04:39:56 by nhayoun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "./mlx/include/MLX42/MLX42.h"
-
-int	main(void)
-{
-	void	*mlx_ptr;
-
-	mlx_ptr = mlx_init(900,900, "SailAway", 0);
-	if (!mlx_ptr)
-		return (1);
-	mlx_loop(mlx_ptr);
-	free(mlx_ptr);
-}
+#ifndef FRACTOL_H
+#define FRACTOL_H
+	#include "./mlx/include/MLX42/MLX42.h"
+#endif
